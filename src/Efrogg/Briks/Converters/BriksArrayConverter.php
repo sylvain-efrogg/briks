5b359b57e86b2e6fac31d51b34a1e155f7f10505
carapuce <carapuce.efrogg@gmail.com>
2017-06-01 14:14:03 +0200

    Ajout d'une fonction de conversion(qui peut etre recursive) de  noeud lorsqu'ils sont composés d'autre noeuds

--- src/Efrogg/Briks/Converters/BriksArrayConverter.php
+++ src/Efrogg/Briks/Converters/BriksArrayConverter.php
@@ -18,56 +18,29 @@ class BriksArrayConverter extends BriksConverter
     public function convert($data)
     {
         foreach ($data['root'] as $key => $values) {
-            $type = new BriksNodeType($key);
-            $type->setTemplate($key . ".twig");
             foreach ($values as $field => $content) {
+                $type = new BriksNodeType($key);
+                $type->setTemplate($key . ".twig");
 
                 // si c'est un groupe d'image alors je parse chaque image
                 if ($field == 'images') {
-
                     $contentField=$this->parseImage($content);
                     $tab[$field] = $contentField;
                     $brik = new BriksNode($tab, $type);
                     return $brik;
-
                 }
 
                 // si le noeud est de type element je dois reconstruire les briks qui le compose
                 if ($field == 'elements') {
-                    foreach ($content as $childField ) {
-
-                        foreach ($childField as $idChildValue => $childValues) {
-                            $type = new BriksNodeType($idChildValue);
-                            $type->setTemplate($idChildValue . ".twig");
-
-                            foreach ( $childValues as $field => $contentChild) {
-                                $contentField = $contentChild;
-
-                                // si c'est un groupe d'image alors je parse chaque image
-                                if ($field == 'images') {
-                                    $contentField = $this->parseImage($contentChild);
-                                }
-                                $tab[$field] = $contentField;
-                            }
-                            $brik[] = new BriksNode($tab, $type);
-                        }
-                    }
-
-                    // reconstitution de l'arbo
-                    $page = new BriksFieldNodeCollection('elements', $brik);
-                    $typePage = new BriksNodeType('page');
-                    $typePage->setTemplate('page' . ".twig");
-                    $brik = new BriksNode([$page], $typePage);
-
-                    return $brik;
+                        return $this->imbricatedNode($content,$field);
                 }
                 $tab[$field] = $content;
             }
             $brik = new BriksNode($tab, $type);
         }
-
         return $brik;
     }
+
 
 
     /**
@@ -85,6 +58,67 @@ class BriksArrayConverter extends BriksConverter
             $images[] = new BriksNode($imgFields, $typeImage);
         }
         return new BriksFieldNodeCollection('images', $images);
+    }
+
+
+
+
+    /**
+     * @param $content
+     * @param null $namePage
+     * @param bool $recurcif
+     * @return BriksNode
+     */
+    public function imbricatedNode($content,$namePage=null,$recurcif=false){
+        foreach ($content as $key=>$childField ) {
+            if(count($childField)>1){
+                $arrayGetName = explode('|', $key);
+                $NewName = $arrayGetName[0];
+                $brik[]=$this->imbricatedNode($childField,$NewName,true);
+            }else {
+                foreach ($childField as $idChildValue => $childValues) {
+                    foreach ($childValues as $field => $contentChild) {
+                        if (substr($idChildValue, 0, 5) == 'image') {
+                            $type = new BriksNodeType('image');
+                            $type->setTemplate("image.twig");
+                        } else {
+                            $type = new BriksNodeType($field);
+                            $type->setTemplate(substr($idChildValue, 0, strlen($idChildValue)) . ".twig");
+                        }
+                        $contentField = $contentChild;
+
+                        // si c'est un groupe d'image alors je parse chaque image
+                        if ($field == 'images') {
+                            $contentField = $this->parseImage($contentChild);
+                        }
+                        $oldField[] = $field;
+                        $tab[$field] = $contentField;
+                    }
+                    $brik[] = new BriksNode($tab, $type);
+                    // clear du tableau pour eviter de cummuler les champs dans les nodes
+                    foreach ($oldField as $ToDeleteField) {
+                        unset($tab[$ToDeleteField]);
+                    }
+                }
+            }
+        }
+
+        // reconstitution de l'arbo
+        $page = new BriksFieldNodeCollection($namePage, $brik);
+
+        // si c'est le premier appel a cette fonction alors je cree le noeud page
+        if($recurcif==false){
+            $typePage = new BriksNodeType('page');
+            $typePage->setTemplate('page' . ".twig");
+        }
+        // sinon je cree les noeuds enfants de page
+        else{
+
+            $typePage = new BriksNodeType($namePage);
+            $typePage->setTemplate($namePage . ".twig");
+        }
+        $returnBrik = new BriksNode([$page], $typePage);
+        return $returnBrik;
     }
 
 }
